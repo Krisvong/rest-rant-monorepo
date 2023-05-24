00e48b80f01efe0db5fc9b55dc9e04c66f952c06
Kristen von Gerichten <kristenvongerichten@kristens-mbp.lan>
2023-05-24 14:10:43 -0400

    completed authoriztion activity

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -1,30 +1,43 @@
 import { useEffect, createContext, useState } from "react";
 
-
-export const CurrentUser = createContext()
+export const CurrentUser = createContext();
 
 function CurrentUserProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState(null);
 
-    const [currentUser, setCurrentUser] = useState(null)
+  useEffect(() => {
+    const getLoggedInUser = async () => {
+      try {
+        const response = await fetch("http://localhost:5001/authentication/profile", {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        });
 
-    useEffect(() => {
-        const getLoggedInUser = async () => {
-            let response = await fetch('http://localhost:5001/authentication/profile', {
-                headers: {
-                    'Authorization': `Bearer ${localStorage.getItem('token')}`
-                }
-            })
-            let user = await response.json()
-            setCurrentUser(user)
+        if (!response.ok) {
+          throw new Error("Failed to fetch user");
         }
-        getLoggedInUser()
-    }, [])
 
-    return (
-        <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
-            {children}
-        </CurrentUser.Provider>
-    )
+        const responseData = await response.text();
+        if (!responseData) {
+          throw new Error("Empty response");
+        }
+
+        const user = JSON.parse(responseData);
+        setCurrentUser(user);
+      } catch (error) {
+        console.error("Error fetching user:", error);
+      }
+    };
+
+    getLoggedInUser();
+  }, []);
+
+  return (
+    <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
+      {children}
+    </CurrentUser.Provider>
+  );
 }
 
-export default CurrentUserProvider
+export default CurrentUserProvider;
